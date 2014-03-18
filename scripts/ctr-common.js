061bd2f11e641893ba2c420b17b56263e2f49693
Alex Deaconu <alex.deaconu@risevision.com>
2014-03-18 11:41:07 -0400

    Added header and common module for the Store

---
+++ scripts/ctr-common.js
@@ -0,0 +1,108 @@
+"use strict";
+
+// Create a module for our core Store services
+var commonModule = angular.module("common", ["ngRoute"]);
+
+commonModule.run(function (apiAuth) { apiAuth.init(); });
+
+commonModule.controller("commonController", ["$scope", "$rootScope", "$sce", "apiAuth", "apiStore", function ($scope, $rootScope, $sce, apiAuth, apiStore) {
+
+    $scope.authStatus = apiAuth.AUTH_STATUS_UNDEFINED; //this value is linked to the UI
+    $scope.userProfilePicture = apiAuth.DEFAULT_PROFILE_PICTURE;
+    $scope.messages = [];
+    $scope.selectedCompanyName = "";
+    $scope.canChangeCompany = false;
+    $scope.companyLoaded = false;
+
+    $scope.updateCartIcon = function () {
+        updateShoopingCartIcon(storageGetProducts().length);
+    };
+
+    $scope.login = function () {
+        apiAuth.login();
+    };
+
+    $scope.logout = function () {
+        apiAuth.logout();
+    };
+
+    $scope.updateAuthStatus = function (value) {
+        if ($scope.authStatus !== value) {
+            $scope.authStatus = value;
+            $scope.userProfileName = apiAuth.userProfileName;
+            $scope.userProfileEmail = apiAuth.userProfileEmail;
+            $scope.userProfilePicture = apiAuth.userProfilePicture;
+        }
+    };
+
+    $scope.getSystemMessages = function () {
+        if (!isPrintView) {
+            if (apiAuth.isAuthed && apiAuth.userCompany) {
+                apiStore.getSystemMessages().then(function (result) { $scope.renderSystemMessages(result); });
+            }
+        }
+    };
+
+    $scope.renderSystemMessages = function (items) {
+        var messages = [];
+        if (items) {
+            for (var i = 0; i < items.length; i++) {
+                messages.push($sce.trustAsHtml(items[i].text));
+            }
+        }
+
+        $scope.messages = messages;
+    };
+
+    $scope.switchCompany = function () {
+        if (apiAuth.isRiseAdmin) {
+            var data = { "onChanged": $scope.setCompany, "companyId": apiAuth.selectedCompany.id };
+            $rootScope.$broadcast("companySelector.open", data);
+        }
+    };
+
+    $scope.setCompany = function (data) {
+        apiAuth.selectedCompany = data.company;
+        $scope.selectedCompanyName = data.company.name;
+        $rootScope.$broadcast("company.changed", data);
+    };
+
+    $scope.resetCompany = function () {
+        if (apiAuth.isRiseAdmin) {
+            $scope.setCompany({ "company": apiAuth.userCompany });
+        }
+    };
+
+    $scope.navigateToHomePageIfNeeded = function () {
+        var addr = document.location.hash;
+        if (addr !== "#/" && addr.substr(0, 10) !== "#/product/") {
+            document.location.hash = "#/";
+        }
+    };
+
+    $scope.$on("profile.loaded", function (event) {
+        $scope.updateAuthStatus(apiAuth.authStatus);
+        $scope.$apply();
+        //$scope.getSystemMessages();
+    });
+
+    $scope.$on("user.signout", function (event) {
+        $scope.companyLoaded = false;
+        $scope.updateAuthStatus(apiAuth.authStatus);
+        $scope.navigateToHomePageIfNeeded();
+        //$apply is needed when user is not logged in when app loads
+        if (!$scope.$$phase) {
+            $scope.$apply();
+        }
+    });
+
+    $scope.$on("userCompany.loaded", function (event) {
+        $scope.companyLoaded = true;
+        $scope.selectedCompanyName = apiAuth.selectedCompany.name;
+        $scope.canChangeCompany = apiAuth.isRiseAdmin;
+        $scope.isPurchaser = apiAuth.isPurchaser;
+        $scope.getSystemMessages();
+    });
+
+}
+]);
