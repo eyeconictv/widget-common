4179499f8a498c14381a998882c2863d41dcf296
Donna Murphy <donna.peplinskie@risevision.com>
2014-03-25 14:20:32 -0400

    Added 'Add Sub-Company', 'Company Users' and 'User Settings' menu items

--- scripts/rise/ctr-common.js
+++ scripts/rise/ctr-common.js
@@ -7,102 +7,106 @@ commonModule.run(function (apiAuth) { apiAuth.init(); });
 
 commonModule.controller("commonController", ["$scope", "$rootScope", "$sce", "apiAuth", "apiStore", function ($scope, $rootScope, $sce, apiAuth, apiStore) {
 
-    $scope.authStatus = apiAuth.AUTH_STATUS_UNDEFINED; //this value is linked to the UI
-    $scope.userProfilePicture = apiAuth.DEFAULT_PROFILE_PICTURE;
-    $scope.messages = [];
-    $scope.selectedCompanyName = "";
-    $scope.canChangeCompany = false;
-    $scope.companyLoaded = false;
+	$scope.authStatus = apiAuth.AUTH_STATUS_UNDEFINED; //this value is linked to the UI
+	$scope.userProfilePicture = apiAuth.DEFAULT_PROFILE_PICTURE;
+	$scope.messages = [];
+	$scope.selectedCompanyName = "";
+	$scope.canChangeCompany = false;
+	$scope.companyLoaded = false;
 
-    $scope.updateCartIcon = function () {
-        updateShoopingCartIcon(storageGetProducts().length);
-    };
+	$scope.updateCartIcon = function () {
+		updateShoopingCartIcon(storageGetProducts().length);
+	};
 
-    $scope.login = function () {
-        apiAuth.login();
-    };
+	$scope.login = function () {
+		apiAuth.login();
+	};
 
-    $scope.logout = function () {
-        apiAuth.logout();
-    };
+	$scope.logout = function () {
+		apiAuth.logout();
+	};
 
-    $scope.updateAuthStatus = function (value) {
-        if ($scope.authStatus !== value) {
-            $scope.authStatus = value;
-            $scope.userProfileName = apiAuth.userProfileName;
-            $scope.userProfileEmail = apiAuth.userProfileEmail;
-            $scope.userProfilePicture = apiAuth.userProfilePicture;
-        }
-    };
+	$scope.updateAuthStatus = function (value) {
+		if ($scope.authStatus !== value) {
+			$scope.authStatus = value;
+			$scope.userProfileName = apiAuth.userProfileName;
+			$scope.userProfileEmail = apiAuth.userProfileEmail;
+			$scope.userProfilePicture = apiAuth.userProfilePicture;
+		}
+	};
 
-    $scope.getSystemMessages = function () {
-        if (!isPrintView) {
-            if (apiAuth.isAuthed && apiAuth.userCompany) {
-                apiStore.getSystemMessages().then(function (result) { $scope.renderSystemMessages(result); });
-            }
-        }
-    };
+	$scope.getSystemMessages = function () {
+		if (!isPrintView) {
+			if (apiAuth.isAuthed && apiAuth.userCompany) {
+				apiStore.getSystemMessages().then(function (result) { $scope.renderSystemMessages(result); });
+			}
+		}
+	};
 
-    $scope.renderSystemMessages = function (items) {
-        var messages = [];
-        if (items) {
-            for (var i = 0; i < items.length; i++) {
-                messages.push($sce.trustAsHtml(items[i].text));
-            }
-        }
+	$scope.renderSystemMessages = function (items) {
+		var messages = [];
+		if (items) {
+			for (var i = 0; i < items.length; i++) {
+				messages.push($sce.trustAsHtml(items[i].text));
+			}
+		}
 
-        $scope.messages = messages;
-    };
+		$scope.messages = messages;
+	};
 
-    $scope.switchCompany = function () {
-        if (apiAuth.isRiseAdmin) {
-            var data = { "onChanged": $scope.setCompany, "companyId": apiAuth.selectedCompany.id };
-            $rootScope.$broadcast("companySelector.open", data);
-        }
-    };
+	$scope.switchCompany = function () {
+		if (apiAuth.isRiseAdmin) {
+			var data = { "onChanged": $scope.setCompany, "companyId": apiAuth.selectedCompany.id };
+			$rootScope.$broadcast("companySelector.open", data);
+		}
+	};
 
-    $scope.setCompany = function (data) {
-        apiAuth.selectedCompany = data.company;
-        $scope.selectedCompanyName = data.company.name;
-        $rootScope.$broadcast("company.changed", data);
-    };
+	$scope.setCompany = function (data) {
+		apiAuth.selectedCompany = data.company;
+		$scope.selectedCompanyName = data.company.name;
+		$rootScope.$broadcast("company.changed", data);
+	};
 
-    $scope.resetCompany = function () {
-        if (apiAuth.isRiseAdmin) {
-            $scope.setCompany({ "company": apiAuth.userCompany });
-        }
-    };
+	$scope.resetCompany = function () {
+		if (apiAuth.isRiseAdmin) {
+			$scope.setCompany({ "company": apiAuth.userCompany });
+		}
+	};
 
-    $scope.navigateToHomePageIfNeeded = function () {
-        var addr = document.location.hash;
-        if (addr !== "#/" && addr.substr(0, 10) !== "#/product/") {
-            document.location.hash = "#/";
-        }
-    };
+	$scope.showUserSettings = function () {
+		$("#company-users-modal").modal("hide");
+		$("#user-settings-modal").modal("show");
+	};
 
-    $scope.$on("profile.loaded", function (event) {
-        $scope.updateAuthStatus(apiAuth.authStatus);
-        $scope.$apply();
-        //$scope.getSystemMessages();
-    });
+	$scope.navigateToHomePageIfNeeded = function () {
+		var addr = document.location.hash;
+		if (addr !== "#/" && addr.substr(0, 10) !== "#/product/") {
+			document.location.hash = "#/";
+		}
+	};
 
-    $scope.$on("user.signout", function (event) {
-        $scope.companyLoaded = false;
-        $scope.updateAuthStatus(apiAuth.authStatus);
-        $scope.navigateToHomePageIfNeeded();
-        //$apply is needed when user is not logged in when app loads
-        if (!$scope.$$phase) {
-            $scope.$apply();
-        }
-    });
+	$scope.$on("profile.loaded", function (event) {
+		$scope.updateAuthStatus(apiAuth.authStatus);
+		$scope.$apply();
+		//$scope.getSystemMessages();
+	});
 
-    $scope.$on("userCompany.loaded", function (event) {
-        $scope.companyLoaded = true;
-        $scope.selectedCompanyName = apiAuth.selectedCompany.name;
-        $scope.canChangeCompany = apiAuth.isRiseAdmin;
-        $scope.isPurchaser = apiAuth.isPurchaser;
-        $scope.getSystemMessages();
-    });
+	$scope.$on("user.signout", function (event) {
+		$scope.companyLoaded = false;
+		$scope.updateAuthStatus(apiAuth.authStatus);
+		$scope.navigateToHomePageIfNeeded();
+		//$apply is needed when user is not logged in when app loads
+		if (!$scope.$$phase) {
+			$scope.$apply();
+		}
+	});
 
+	$scope.$on("userCompany.loaded", function (event) {
+		$scope.companyLoaded = true;
+		$scope.selectedCompanyName = apiAuth.selectedCompany.name;
+		$scope.canChangeCompany = apiAuth.isRiseAdmin;
+		$scope.isPurchaser = apiAuth.isPurchaser;
+		$scope.getSystemMessages();
+	});
 }
 ]);
