6b88ce92a4004a7224b1e253053b087d4915566d
Stuart Lees <stuartlees@gmail.com>
2014-05-15 20:05:08 -0500

    Optimized the validation module in common.js
    - Changed namespace from Settings to Validation
    - Removed dependency on jQuery
    - Refactored functions to be pure JS
    - Added greaterThan and lessThan public functions

--- scripts/rise/common.js
+++ scripts/rise/common.js
@@ -2,36 +2,38 @@ var RiseVision = RiseVision || {};
 
 RiseVision.Common = RiseVision.Common || {};
 
-RiseVision.Common.Settings = (function($, i18n) {
-  /*
-  Defining validation messages
-   */
-  var messages = {
-    required: ' is a required field.',
-    numeric: ' must contain only numbers.',
-    valid_url: ' must contain a valid URL.'
-  };
+RiseVision.Common.Validation = (function() {
+  "use strict";
 
   /*
   Defining the regular expressions being used
    */
   var urlRegExp = /^(?:(?:ht|f)tp(?:s?)\:\/\/|~\/|\/)?(?:\w+:\w+@)?((?:(?:[-\w\d{1-3}]+\.)+(?:com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|edu|co\.uk|ac\.uk|it|fr|tv|museum|asia|local|travel|[a-z]{2}))|((\b25[0-5]\b|\b[2][0-4][0-9]\b|\b[0-1]?[0-9]?[0-9]\b)(\.(\b25[0-5]\b|\b[2][0-4][0-9]\b|\b[0-1]?[0-9]?[0-9]\b)){3}))(?::[\d]{1,5})?(?:(?:(?:\/(?:[-\w~!$+|.,=]|%[a-f\d]{2})+)+|\/)+|\?|#)?(?:(?:\?(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)(?:&(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)*)*(?:#(?:[-\w~!$ |\/.,*:;=]|%[a-f\d]{2})*)?$/i,
-      numericRegex = /^(\-|\+)?([0-9]+|Infinity)$/;
+      numericRegex = /^(\-|\+)?([0-9]+|Infinity)$/,
+      decimalRegex = /^\-?[0-9]*\.?[0-9]+$/;
 
-  /*
-   A check on an element to see if it's visible
-   */
-  function isElementVisible($element){
-   return ($element.is(":visible"));
+  function isGreaterThan(element, param) {
+    var value = element.value.trim();
+
+    if (!decimalRegex.test(value)) {
+      return false;
+    }
+
+    return (parseFloat(value) > parseFloat(param));
   }
 
-  function isValidNumber($element, errors, fieldName){
-    var val = $.trim($element.val());
+  function isLessThan(element, param) {
+    var value = element.value.trim();
 
-    // Don't validate element if it's hidden.
-    if (!isElementVisible($element)) {
-      return true;
+    if (!decimalRegex.test(value)) {
+      return false;
     }
+
+    return (parseFloat(value) < parseFloat(param));
+  }
+
+  function isValidNumber(element){
+    var value = element.value.trim();
 
     /*
      Regexp being used is stricter than parseInt. Using regular expression as
@@ -39,26 +41,15 @@ RiseVision.Common.Settings = (function($, i18n) {
      https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/
      Global_Objects/parseInt
      */
-
-    if (!numericRegex.test(val)) {
-      errors.innerHTML += fieldName + " must be a number.<br />";
-      return false;
-    }
-
-    return true;
+    return numericRegex.test(value);
   }
 
-  function isValidRequired($element, errors, fieldName){
-    //Don't validate element if it's hidden.
-    if (!isElementVisible($element)) {
-      return true;
-    }
-
-    var value = $.trim($element.val()),
+  function isValidRequired(element){
+    var value = element.value.trim(),
         valid = false;
 
-    if ($element.is('checkbox') || $element.is('radio')) {
-      if($element.is(":checked") === true) {
+    if (element.type === "checkbox" || element.type === "radio") {
+      if(element.checked === true) {
         valid = true;
       }
     } else {
@@ -67,20 +58,11 @@ RiseVision.Common.Settings = (function($, i18n) {
       }
     }
 
-    if (!valid) {
-      errors.innerHTML += fieldName + messages.required + "<br />";
-      return false;
-    }
-
-    return true;
+    return valid;
   }
 
-  function isValidURL($element, errors, fieldName){
-    //Don't validate element if it's hidden.
-    if (!isElementVisible($element)) {
-      return true;
-    }
-
+  function isValidURL(element){
+    var value = element.value.trim();
     /*
      Discussion
      http://stackoverflow.com/questions/37684/how-to-replace-plain-urls-
@@ -90,17 +72,14 @@ RiseVision.Common.Settings = (function($, i18n) {
      https://github.com/component/regexps/blob/master/index.js#L3
 
      */
-    if (!urlRegExp.test($.trim($element.val()))){
-      errors.innerHTML += fieldName + messages.valid_url + "<br />";
-      return false;
-    }
-
-    return true;
+    return urlRegExp.test(value);
   }
 
   return {
-    validateRequired: isValidRequired,
-    validateURL: isValidURL,
-    validateNumber: isValidNumber
+    greaterThan: isGreaterThan,
+    lessThan: isLessThan,
+    required: isValidRequired,
+    url: isValidURL,
+    numeric: isValidNumber
   }
-})($, i18n);
+})();
