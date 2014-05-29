6307a58501f9b5962ac0cd1a5acb0312b04c9ece
Donna Murphy <donna.peplinskie@risevision.com>
2014-05-29 15:05:45 -0400

    Shorten line length

--- scripts/bootstrap-wysihtml5-extender/wysihtml5-extender.js
+++ scripts/bootstrap-wysihtml5-extender/wysihtml5-extender.js
@@ -1,67 +1,85 @@
 /* Standard Font */
 (function(wysihtml5) {
-	var REG_EXP = /wysiwyg-font-family-[a-z\-]+/g;
+  var REG_EXP = /wysiwyg-font-family-[a-z\-]+/g;
 
-	wysihtml5.commands.standardFont = {
-			exec: function(composer, command, font, fontFamily, attrs) {
-				var className = "wysiwyg-font-family-" + font.replace(/ /g, "-").toLowerCase();
-				var style = document.createElement("style");
+  wysihtml5.commands.standardFont = {
+      exec: function(composer, command, font, fontFamily, attrs) {
+        var className = "wysiwyg-font-family-" + font.replace(/ /g, "-")
+          .toLowerCase();
+        var style = document.createElement("style");
 
-				// Add a CSS class for the selected font.
-				style.type = "text/css";
-				style.innerHTML = "." + className +" { font-family: " + fontFamily + "; }";
-				composer.iframe.contentDocument.getElementsByTagName("head")[0].appendChild(style);
+        // Add a CSS class for the selected font.
+        style.type = "text/css";
+        style.innerHTML = "." + className +" { font-family: " + fontFamily +
+          "; }";
+        composer.iframe.contentDocument.getElementsByTagName("head")[0]
+          .appendChild(style);
 
-				return wysihtml5.commands.formatInline.exec(composer, command, "span", className, REG_EXP, attrs);
-			},
-			state: function(composer, command, font, fontFamily, attrs) {
-				var className = "wysiwyg-font-family-" + font.replace(/ /g, "-").toLowerCase();
+        return wysihtml5.commands.formatInline.exec(composer, command, "span",
+          className, REG_EXP, attrs);
+      },
+      state: function(composer, command, font, fontFamily, attrs) {
+        var className = "wysiwyg-font-family-" + font.replace(/ /g, "-")
+          .toLowerCase();
 
-				return wysihtml5.commands.formatInline.state(composer, command, "span", className, REG_EXP, attrs);
-			}
-		};
+        return wysihtml5.commands.formatInline.state(composer, command, "span",
+          className, REG_EXP, attrs);
+      }
+    };
 })(wysihtml5);
 
 /* Google Font */
 (function(wysihtml5) {
-	var REG_EXP = /wysiwyg-font-family-[a-z\-]+/g;
+  var REG_EXP = /wysiwyg-font-family-[a-z\-]+/g;
 
-	wysihtml5.commands.googleFont = {
-			exec: function(composer, command, font, attrs) {
-				var className = "wysiwyg-font-family-" + font.replace(/ /g, "-").toLowerCase();
-				var style = document.createElement("style");
+  wysihtml5.commands.googleFont = {
+      exec: function(composer, command, font, attrs) {
+        var className = "wysiwyg-font-family-" + font.replace(/ /g, "-")
+          .toLowerCase();
+        var style = document.createElement("style");
 
-				// Add CSS for the selected font plus a fallback.
-				style.type = "text/css";
-				style.innerHTML = "." + className +" { font-family: '" + font + "', serif; }";
-				composer.iframe.contentDocument.getElementsByTagName("head")[0].appendChild(style);
+        // Add CSS for the selected font plus a fallback.
+        style.type = "text/css";
+        style.innerHTML = "." + className +" { font-family: '" + font +
+          "', serif; }";
+        composer.iframe.contentDocument.getElementsByTagName("head")[0]
+          .appendChild(style);
 
-				return wysihtml5.commands.formatInline.exec(composer, command, "span", className, REG_EXP, attrs);
-			},
-			state: function(composer, command, font, attrs) {
-				return wysihtml5.commands.formatInline.state(composer, command, "span", "wysiwyg-font-family-" + font.replace(/ /g, "-").toLowerCase(), REG_EXP, attrs);
-			}
-		};
+        return wysihtml5.commands.formatInline.exec(composer, command, "span",
+          className, REG_EXP, attrs);
+      },
+      state: function(composer, command, font, attrs) {
+        return wysihtml5.commands.formatInline.state(composer, command, "span",
+          "wysiwyg-font-family-" + font.replace(/ /g, "-").toLowerCase(),
+          REG_EXP, attrs);
+      }
+    };
 })(wysihtml5);
 
 /* Custom Font */
 (function(wysihtml5) {
-	var REG_EXP = /wysiwyg-font-family-[a-z\-]+/g;
+  var REG_EXP = /wysiwyg-font-family-[a-z\-]+/g;
 
-	wysihtml5.commands.customFont = {
-			exec: function(composer, command, font, attrs) {
-				var className = "wysiwyg-font-family-" + font.replace(/ /g, "-").toLowerCase();
-				var style = document.createElement("style");
+  wysihtml5.commands.customFont = {
+      exec: function(composer, command, font, attrs) {
+        var className = "wysiwyg-font-family-" + font.replace(/ /g, "-")
+          .toLowerCase();
+        var style = document.createElement("style");
 
-				// Add CSS for the selected font plus a fallback.
-				style.type = "text/css";
-				style.innerHTML = "." + className +" { font-family: '" + font + "', serif; }";
-				composer.iframe.contentDocument.getElementsByTagName("head")[0].appendChild(style);
+        // Add CSS for the selected font plus a fallback.
+        style.type = "text/css";
+        style.innerHTML = "." + className +" { font-family: '" + font +
+          "', serif; }";
+        composer.iframe.contentDocument.getElementsByTagName("head")[0]
+          .appendChild(style);
 
-				return wysihtml5.commands.formatInline.exec(composer, command, "span", className, REG_EXP, attrs);
-			},
-			state: function(composer, command, font, attrs) {
-				return wysihtml5.commands.formatInline.state(composer, command, "span", "wysiwyg-font-family-" + font.replace(/ /g, "-").toLowerCase(), REG_EXP, attrs);
-			}
-		};
+        return wysihtml5.commands.formatInline.exec(composer, command, "span",
+          className, REG_EXP, attrs);
+      },
+      state: function(composer, command, font, attrs) {
+        return wysihtml5.commands.formatInline.state(composer, command, "span",
+          "wysiwyg-font-family-" + font.replace(/ /g, "-").toLowerCase(),
+          REG_EXP, attrs);
+      }
+    };
 })(wysihtml5);
