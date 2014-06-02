6e824f0876c69bb7b8acd0f89acba272c5f8dbcb
Donna Murphy <donna.peplinskie@risevision.com>
2014-06-02 11:06:38 -0400

    Convert indentation to spaces

--- scripts/bootstrap-font-size-picker/bootstrap-font-size-picker.js
+++ scripts/bootstrap-font-size-picker/bootstrap-font-size-picker.js
@@ -3,59 +3,59 @@
  *  (reproduced in the LICENSE file).
  */
  ;(function ($, window, document, undefined) {
-	"use strict";
+  "use strict";
 
-	var _pluginName = "fontSizePicker";
+  var _pluginName = "fontSizePicker";
 
-	function Plugin(element, options) {
-		var $element = $(element);
+  function Plugin(element, options) {
+    var $element = $(element);
 
-		options = $.extend({}, { "font-size": "14" }, options);
+    options = $.extend({}, { "font-size": "14" }, options);
 
-		/*
-		 *  Private Methods
-		 */
-		function _init() {
-			// Add the markup.
-			$element.append(
-				"<div class='bfh-selectbox'>" +
-					"<select class='form-control bfh-fontsizes'></select>" +
-				"</div>");
+    /*
+     *  Private Methods
+     */
+    function _init() {
+      // Add the markup.
+      $element.append(
+        "<div class='bfh-selectbox'>" +
+          "<select class='form-control bfh-fontsizes'></select>" +
+        "</div>");
 
-			// Initialize the font size picker component.
-			$element.find(".bfh-fontsizes").bfhfontsizes({
-				"size": options["font-size"]
-			}).selectpicker();
-		}
+      // Initialize the font size picker component.
+      $element.find(".bfh-fontsizes").bfhfontsizes({
+        "size": options["font-size"]
+      }).selectpicker();
+    }
 
-		/*
-		 *  Public Methods
-		 */
-		function getSize() {
-			return $element.find(".font-size").val();
-	}
+    /*
+     *  Public Methods
+     */
+    function getSize() {
+      return $element.find(".font-size").val();
+  }
 
-		function setSize(size) {
-			$element.find(".bfh-fontsizes").selectpicker("val", parseInt(size, 10));
-		}
+    function setSize(size) {
+      $element.find(".bfh-fontsizes").selectpicker("val", parseInt(size, 10));
+    }
 
-		_init();
+    _init();
 
-		return {
-			getFontSize: getSize,
-			setFontSize: setSize
-		};
-	}
+    return {
+      getFontSize: getSize,
+      setFontSize: setSize
+    };
+  }
 
-	/*
-	 *  A lightweight plugin wrapper around the constructor that prevents
-	 *  multiple instantiations.
-	 */
-	$.fn.fontSizePicker = function(options) {
-		return this.each(function() {
-			if (!$.data(this, "plugin_" + _pluginName)) {
-				$.data(this, "plugin_" + _pluginName, new Plugin(this, options));
-			}
-		});
-	};
+  /*
+   *  A lightweight plugin wrapper around the constructor that prevents
+   *  multiple instantiations.
+   */
+  $.fn.fontSizePicker = function(options) {
+    return this.each(function() {
+      if (!$.data(this, "plugin_" + _pluginName)) {
+        $.data(this, "plugin_" + _pluginName, new Plugin(this, options));
+      }
+    });
+  };
 })(jQuery, window, document);
