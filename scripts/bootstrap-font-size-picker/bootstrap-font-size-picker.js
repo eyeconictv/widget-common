6b3bc2667f3aad784fe537f520698a259d2f3c69
Donna Murphy <donna.peplinskie@risevision.com>
2014-06-02 11:05:53 -0400

    Set current font size
    Add ability to set font size
    Change default font size from 11 to 14
    Call selectpicker() to style drop down

--- scripts/bootstrap-font-size-picker/bootstrap-font-size-picker.js
+++ scripts/bootstrap-font-size-picker/bootstrap-font-size-picker.js
@@ -1,6 +1,6 @@
-/*	Copyright © 2014 Rise Vision Incorporated.
- *	Use of this software is governed by the GPLv3 license
- *	(reproduced in the LICENSE file).
+/*  Copyright © 2014 Rise Vision Incorporated.
+ *  Use of this software is governed by the GPLv3 license
+ *  (reproduced in the LICENSE file).
  */
  ;(function ($, window, document, undefined) {
 	"use strict";
@@ -10,41 +10,46 @@
 	function Plugin(element, options) {
 		var $element = $(element);
 
-		options = $.extend({}, { "font-size": "11" }, options);
+		options = $.extend({}, { "font-size": "14" }, options);
 
 		/*
-		 *	Private Methods
+		 *  Private Methods
 		 */
 		function _init() {
 			// Add the markup.
 			$element.append(
 				"<div class='bfh-selectbox'>" +
-					"<select class='form-control selectpicker bfh-fontsizes'></select>" +
+					"<select class='form-control bfh-fontsizes'></select>" +
 				"</div>");
 
 			// Initialize the font size picker component.
 			$element.find(".bfh-fontsizes").bfhfontsizes({
 				"size": options["font-size"]
-			});
+			}).selectpicker();
 		}
 
 		/*
-		 *	Public Methods
+		 *  Public Methods
 		 */
 		function getSize() {
 			return $element.find(".font-size").val();
+	}
+
+		function setSize(size) {
+			$element.find(".bfh-fontsizes").selectpicker("val", parseInt(size, 10));
 		}
 
 		_init();
 
 		return {
-			getFontSize: getSize
+			getFontSize: getSize,
+			setFontSize: setSize
 		};
 	}
 
 	/*
-	 *	A lightweight plugin wrapper around the constructor that prevents
-	 *	multiple instantiations.
+	 *  A lightweight plugin wrapper around the constructor that prevents
+	 *  multiple instantiations.
 	 */
 	$.fn.fontSizePicker = function(options) {
 		return this.each(function() {
