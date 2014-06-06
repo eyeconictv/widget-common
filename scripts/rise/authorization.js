7207a64c0e4158867b50d370979d3a38e9a50f0a
Stuart Lees <stuartlees@gmail.com>
2014-06-06 13:19:56 -0400

    Abstraction and optimization
    - More public methods for flexibility of use
    - Abstracting auth api
    - No window events to trigger
    - Removal of jQuery

--- scripts/rise/authorization.js
+++ scripts/rise/authorization.js
@@ -1,46 +1,59 @@
 var RiseVision = RiseVision || {};
 
 // check if dependencies exist
-if (typeof jQuery === 'undefined' || typeof gapi === 'undefined') {
-
+if (typeof gapi === 'undefined') {
   throw new Error("authorization.js dependencies not loaded");
-  
 } else {
   // dependencies exist, create module
-  RiseVision.Authorization = (function($, gapi) {
+  RiseVision.Authorization = (function(gapi) {
     "use strict";
 
     // Constants
     var CLIENT_ID = "726689182011.apps.googleusercontent.com";
 
     // Private vars
-    var oauthToken = null;
+    var oauthToken = null, loaded = false;
 
-    function checkAuth(immediate, scope){
+    function authorize(immediate, scope, callbackFn){
       gapi.auth.authorize({
         client_id : CLIENT_ID,
         scope : scope,
         immediate : immediate
-      }, onAuthResult);
+      }, function(authResult){
+        if (authResult && !authResult.error) {
+          oauthToken = authResult.access_token;
+        } else {
+          if(window.console){
+            console.info("Authorization Fail: " + authResult.error);
+          }
+        }
+        callbackFn.call(null,oauthToken);
+      });
+    }
+
+    function isApiLoaded(){
+      return loaded;
+    }
+
+    function loadApi(callbackFn){
+      // Use the API Loader script to load the Authentication script.
+      gapi.load('auth', {'callback': function(){
+        loaded = true;
+        if(typeof callbackFn === 'function'){
+          callbackFn.apply(null);
+        }
+      }});
     }
 
     function getAuthToken(){
       return oauthToken;
     }
 
-    function onAuthResult(authResult){
-      if (authResult && !authResult.error) {
-        oauthToken = authResult.access_token;
-        $(window).trigger("gapi_auth_success");
-      } else {
-        $(window).trigger("gapi_auth_failure", [authResult.error]);
-      }
-    }
-
     return {
-      checkAuth: checkAuth,
-      getAuthToken: getAuthToken
+      authorize: authorize,
+      getAuthToken: getAuthToken,
+      isApiLoaded: isApiLoaded,
+      loadApi: loadApi
     };
-  })(jQuery, gapi);
+  })(gapi);
 }
-
