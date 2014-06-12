3848d10eacec20354663379a65cda91131351780
Donna Murphy <donna.peplinskie@risevision.com>
2014-06-12 09:46:51 -0400

    Colour pickers
    Add support for text highlight and background colours

--- scripts/bootstrap-wysihtml5-extender/wysihtml5-extender.js
+++ scripts/bootstrap-wysihtml5-extender/wysihtml5-extender.js
@@ -18,13 +18,6 @@
       return wysihtml5.commands.formatInline.exec(composer, command, "span",
         className, REG_EXP, attrs);
     },
-    state: function(composer, command, font, fontFamily, attrs) {
-      var className = "wysiwyg-font-family-" + font.replace(/ /g, "-")
-        .toLowerCase();
-
-      return wysihtml5.commands.formatInline.state(composer, command, "span",
-        className, REG_EXP, attrs);
-    }
   };
 })(wysihtml5);
 
@@ -48,11 +41,6 @@
       return wysihtml5.commands.formatInline.exec(composer, command, "span",
         className, REG_EXP, attrs);
     },
-    state: function(composer, command, font, attrs) {
-      return wysihtml5.commands.formatInline.state(composer, command, "span",
-        "wysiwyg-font-family-" + font.replace(/ /g, "-").toLowerCase(),
-        REG_EXP, attrs);
-    }
   };
 })(wysihtml5);
 
@@ -76,11 +64,6 @@
       return wysihtml5.commands.formatInline.exec(composer, command, "span",
         className, REG_EXP, attrs);
     },
-    state: function(composer, command, font, attrs) {
-      return wysihtml5.commands.formatInline.state(composer, command, "span",
-        "wysiwyg-font-family-" + font.replace(/ /g, "-").toLowerCase(),
-        REG_EXP, attrs);
-    }
   };
 })(wysihtml5);
 
@@ -102,11 +85,61 @@
       return wysihtml5.commands.formatInline.exec(composer, command, "span",
         className, REG_EXP, attrs);
     },
-    state: function(composer, command, color, attrs) {
-      var className = "wysiwyg-text-color-" + color.replace("#", "");
+  };
+})(wysihtml5);
 
-      return wysihtml5.commands.formatInline.state(composer, command, "span",
+/* Text Highlight Color */
+(function(wysihtml5) {
+  var REG_EXP = /wysiwyg-highlight-color-[a-z0-9\-]+/g;
+
+  wysihtml5.commands.highlightColor = {
+    exec: function(composer, command, color, attrs) {
+      var className = "wysiwyg-highlight-color-" + color.replace("#", "");
+      var style = document.createElement("style");
+
+      // Add a CSS class for the selected font.
+      style.type = "text/css";
+      style.innerHTML = "." + className +" { background-color: " + color + "; }";
+      composer.iframe.contentDocument.getElementsByTagName("head")[0]
+        .appendChild(style);
+
+      return wysihtml5.commands.formatInline.exec(composer, command, "span",
         className, REG_EXP, attrs);
-    }
+    },
+  };
+})(wysihtml5);
+
+/* Background Color */
+(function(wysihtml5) {
+  var REG_EXP = /wysiwyg-background-color-[a-z0-9\-]+/g;
+
+  wysihtml5.commands.backgroundColor = {
+    exec: function(composer, command, color, attrs) {
+      var className = "wysiwyg-background-color-" + color.replace("#", "");
+      var style = document.createElement("style");
+      var body = editor.composer.doc.body;
+      var classes = body.classList;
+
+      // Add a CSS class for the selected font. Unfortunately, !important is
+      // needed to override the inline style.
+      style.type = "text/css";
+      style.innerHTML = "." + className + " { background-color: " + color +
+        " !important; }";
+      composer.iframe.contentDocument.getElementsByTagName("head")[0]
+        .appendChild(style);
+
+      // Remove any old background class before adding the new class.
+      for (var i = 0; i < classes.length; i++) {
+        if (classes[i].match(REG_EXP)) {
+          body.classList.remove(classes[i]);
+        }
+      }
+
+      for (i = 0; i < attrs.length; i++) {
+        body.setAttribute(attrs[i].name, attrs[i].value);
+      }
+
+      body.classList.add(className);
+    },
   };
 })(wysihtml5);
