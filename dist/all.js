aac4562108092582eb7d93e755f452fbab3ecb43
Alex Deaconu <alex.deaconu@risevision.com>
2014-09-24 11:22:46 -0400

    [feature] Store Authorization implementation
    Implemented as per http://www.risevision.com/help/developers/store-authorization/
    
    Updated config values
    Fixed some lint errors
    Added css for authorization errors and corresponding gulp task

--- dist/all.js
+++ dist/all.js
@@ -156,7 +156,7 @@ RiseVision.Common.Utilities = (function() {
 		var sheet = null;
 		var rule = "font-family: " + family + "; " + "src: url('" + url + "');";
 
-		if (contentDocument == null) {
+		if (contentDocument === null) {
 			contentDocument = document;
 		}
 
@@ -168,7 +168,7 @@ RiseVision.Common.Utilities = (function() {
 	}
 
 	function loadGoogleFont(family, contentDocument) {
-		if (contentDocument == null) {
+		if (contentDocument === null) {
 			contentDocument = document;
 		}
 
@@ -189,8 +189,9 @@ RiseVision.Common.Utilities = (function() {
 		loadGoogleFont: loadGoogleFont,
 	};
 })();
-var CONFIG = {
-  FINANCIAL_SERVER_URL: "http://contentfinancial2-test.appspot.com/"
+var WIDGET_COMMON_CONFIG = {
+  STORE_URL: "https://store-dot-rvaserver2.appspot.com/",
+  AUTH_PATH_URL: "v1/widget/auth",
 };
 
 var RiseVision = RiseVision || {};
@@ -866,6 +867,112 @@ var CollectionTimes = (function() {
     }
   };
 })();
+// Implements http://www.risevision.com/help/developers/store-authorization/
+
+var RiseVision = RiseVision || {};
+RiseVision.Common = RiseVision.Common || {};
+
+RiseVision.Common.Store = RiseVision.Common.Store || {};
+RiseVision.Common.Store.Auth = {};
+
+RiseVision.Common.Store.Auth = function() {
+  var backDrop, warningDialog;
+  this.callback = null;
+  this.authorized = true;
+  
+  this.checkForDisplay = function(displayId, productCode, callback) {
+    this.callback = callback;
+    this.url = WIDGET_COMMON_CONFIG.STORE_URL +
+              WIDGET_COMMON_CONFIG.AUTH_PATH_URL + 
+              "?id=" + displayId + "&pc=" + productCode + "";
+              
+    this.callApi();
+  };
+ 
+  this.checkForCompany = function(companyId, productCode, callback) {
+    this.callback = callback;
+    this.url = WIDGET_COMMON_CONFIG.STORE_URL +
+              WIDGET_COMMON_CONFIG.AUTH_PATH_URL + 
+              "?cid=" + companyId + "&pc=" + productCode + "";
+              
+    this.callApi(url);
+  };
+  
+  this.callApi = function() {
+    var self = this;
+
+    $.ajax({
+      dataType: "json",
+      url: this.url,
+      success: function(data, textStatus) {
+        self.onSuccess(data, textStatus);
+      },
+      error: function() {
+        self.onError();
+      }
+    });
+  };
+  
+  this.onSuccess = function(data, textStatus) {
+    var self = this;
+    if (data && data.authorized) {
+      this.authorized = true;
+      
+      hideNotification();
+      
+      // check again for authorization one hour before it expires
+      var milliSeconds = new Date(data.expiry).getTime() - new Date().getTime() - 60 * 60 * 1000;
+      setTimeout(this.callApi, milliSeconds);
+    }
+    else if (data && !data.authorized) {
+      this.authorized = false;
+      
+      showNotification("Product not authorized.");
+      
+      // check authoriztation every hour if failed
+      setTimeout(this.callApi, 60 * 60 * 1000);
+    }
+    else {
+      // API failed, try again in an hour
+      setTimeout(this.callApi, 60 * 60 * 1000);      
+    }
+
+    if (this.callback) {
+      this.callback(this.authorized);
+    }
+  };
+  
+  this.onError = function() {
+    this.authorized = false;
+    
+    showNotification("Cannot connect to Store for authorization.");
+    
+    // check authoriztation every hour if failed
+    setTimeout(this.callApi, 60 * 60 * 1000);
+    
+    if (this.callback) {
+      this.callback(this.authorized);
+    }
+  };
+  
+  function showNotification(message) {
+    backDrop = document.createElement("div");
+    backDrop.className = "overlay";
+    document.body.appendChild(backDrop);
+
+    warningDialog = document.createElement("div");
+    warningDialog.className = "auth-warning";
+    warningDialog.innerHTML = message;
+    warningDialog = document.body.appendChild(warningDialog);
+  } 
+  
+  function hideNotification() {
+    if (backDrop && warningDialog) {
+      warningDialog.parentNode.removeChild(warningDialog);
+      backDrop.parentNode.removeChild(backDrop);    
+    }
+  }
+};
 var RiseVision = RiseVision || {};
 RiseVision.Common = RiseVision.Common || {};
 
--- dist/config.js
+++ dist/config.js
@@ -1,3 +1,4 @@
-var CONFIG = {
-  FINANCIAL_SERVER_URL: "http://contentfinancial2-test.appspot.com/"
+var WIDGET_COMMON_CONFIG = {
+  STORE_URL: "https://store-dot-rvaserver2.appspot.com/",
+  AUTH_PATH_URL: "v1/widget/auth",
 };
