ea75047ba58251632356063d4d5ac9dae015d3e9
Donna Peplinskie <donna.peplinskie@risevision.com>
2015-10-16 14:49:31 -0400

    Add Widget logging
    Add LoggerUtils module to setup unit testing on reoccuring functions
    
    Add optional callback to `log()` for retrieving response body
    
    Add unit tests
    
    Add data type tests for `getInsertData` unit tests
    
    Use constants in unit tests

--- dist/config.js
+++ dist/config.js
@@ -5,6 +5,9 @@ var WIDGET_COMMON_CONFIG = {
 
 if (typeof config === "undefined") {
   var config = {
-    STORAGE_ENV: "prod"
+    STORAGE_ENV: "prod",
+    LOGGER_CLIENT_ID: "1088527147109-6q1o2vtihn34292pjt4ckhmhck0rk0o7.apps.googleusercontent.com",
+    LOGGER_CLIENT_SECRET: "nlZyrcPLg6oEwO9f9Wfn29Wh",
+    LOGGER_REFRESH_TOKEN: "1/xzt4kwzE1H7W9VnKB8cAaCx6zb4Es4nKEoqaYHdTD15IgOrJDtdun6zK6XiATCKT"
   };
 }
---
+++ test/unit/logger-spec.js
@@ -0,0 +1,133 @@
+"use strict";
+
+var utils = RiseVision.Common.LoggerUtils,
+  event = "load",
+  displayId = "abc123",
+  eventDetails = "Widget loaded";
+
+describe("getInsertData", function() {
+  var data = null;
+
+  before(function() {
+    data = utils.getInsertData(event, displayId, eventDetails);
+  });
+
+  it("should return an object containing insertId property", function() {
+    expect(data.rows[0].insertId).to.exist;
+    expect(data.rows[0].insertId).to.be.a("string");
+  });
+
+  it("should return an object containing event property", function() {
+    expect(data.rows[0].json.event).to.exist;
+    expect(data.rows[0].json.event).to.be.a("string");
+    expect(data.rows[0].json.event).to.equal(event);
+  });
+
+  it("should return an object containing display_id property", function() {
+    expect(data.rows[0].json.display_id).to.exist;
+    expect(data.rows[0].json.display_id).to.be.a("string");
+    expect(data.rows[0].json.display_id).to.equal(displayId);
+  });
+
+  it("should return an object containing ts property", function() {
+    expect(data.rows[0].json.ts).to.exist;
+    expect(data.rows[0].json.ts).to.be.a("string");
+  });
+
+  it("should return an object containing event_details property", function() {
+    expect(data.rows[0].json.event_details).to.exist;
+    expect(data.rows[0].json.event_details).to.be.a("string");
+    expect(data.rows[0].json.event_details).to.equal(eventDetails);
+  });
+
+  it("should set event_details property to an empty string", function() {
+    data = utils.getInsertData(event, displayId);
+
+    expect(data.rows[0].json.event_details).to.equal("");
+  });
+});
+
+describe("getTable", function() {
+  it("should return the correct table name", function() {
+    var tableName = "test",
+      date = new Date(),
+      year = date.getUTCFullYear(),
+      month = date.getUTCMonth() + 1,
+      day = date.getUTCDate();
+
+    if (month < 10) {
+      month = "0" + month;
+    }
+
+    if (day < 10) {
+      day = "0" + day;
+    }
+
+    expect(utils.getTable(tableName)).to.equal(tableName + year + month + day);
+  });
+});
+
+describe("log", function() {
+  var logger = RiseVision.Common.Logger,
+    interval = 3580000,
+    token = "my-token",
+    data = { "access_token": token },
+    json = JSON.stringify(data),
+    xhr, clock, requests;
+
+  before(function() {
+    xhr = sinon.useFakeXMLHttpRequest();
+
+    xhr.onCreate = function (xhr) {
+      requests.push(xhr);
+    };
+
+    clock = sinon.useFakeTimers();
+  });
+
+  beforeEach(function() {
+    requests = [];
+
+    clock.tick(interval);
+    logger.log(event, displayId, eventDetails);
+
+    // Respond to refresh token request.
+    requests[0].respond(200, { "Content-Type": "text/json" }, json);
+  });
+
+  after(function() {
+    xhr.restore();
+    clock.restore();
+  });
+
+  it("should make a POST request", function() {
+    expect(requests[1].method).to.equal("POST");
+  });
+
+  it("should make a request to the correct URL", function() {
+    expect(requests[1].url).to.contain("https://www.googleapis.com/bigquery/v2/projects/client-side-events/datasets/Widget_Events/tables/events");
+    expect(requests[1].url).to.contain("/insertAll");
+  });
+
+  it("should set the Content-Type header", function() {
+    expect(requests[1].requestHeaders["Content-Type"]).to.equal("application/json;charset=utf-8");
+  });
+
+  it("should set the Authorization header", function() {
+    expect(requests[1].requestHeaders.Authorization).to.equal("Bearer " + token);
+  });
+
+  it("should send string data in the body", function() {
+    expect(requests[1].requestBody).to.contain('{"kind":"bigquery#tableDataInsertAllRequest","skipInvalidRows":false,"ignoreUnknownValues":false,"rows":[{"insertId":');
+    expect(requests[1].requestBody).to.contain('"json":{"event":"' + event + '","display_id":"' + displayId + '","event_details":"' + eventDetails + '","ts":');
+  });
+
+  it("should not make a request if event is empty", function() {
+    requests = [];
+
+    clock.tick(interval);
+    logger.log("", displayId, eventDetails);
+
+    expect(requests.length).to.equal(0);
+  });
+});
