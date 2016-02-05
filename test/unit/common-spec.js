112ef48d00f1b752b69de75d3ac67b894018d5c3
rodrigopavezi <rodrigopavezi@gmail.com>
2016-02-05 12:18:08 -0200

    Added common method to retrieve the error message for rise cache errors

--- test/unit/common-spec.js
+++ test/unit/common-spec.js
@@ -20,4 +20,34 @@ describe("getting query parameters", function() {
 
     expect(value).to.equal("abc123");
   });
+
+  it("returns error message for rise cache 404", function() {
+    var utils = RiseVision.Common.Utilities,
+      value,
+      status = 404;
+
+    value = utils.getRiseCacheErrorMessage(status);
+
+    expect(value).to.equal("The file does not exist or cannot be accessed.");
+  });
+
+  it("returns error message for rise cache 507", function() {
+    var utils = RiseVision.Common.Utilities,
+      value,
+      status = 507;
+
+    value = utils.getRiseCacheErrorMessage(status);
+
+    expect(value).to.equal("There is not enough disk space to save the file on Rise Cache.");
+  });
+
+  it("returns error message for rise cache default error", function() {
+    var utils = RiseVision.Common.Utilities,
+      value,
+      status = 500;
+
+    value = utils.getRiseCacheErrorMessage(status);
+
+    expect(value).to.equal("There was a problem retrieving the file from Rise Cache.");
+  });
 });
