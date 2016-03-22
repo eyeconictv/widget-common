98c91d9b58c6a2c291fe47f1206fdbd8854f63e2
Donna Peplinskie <donna.peplinskie@risevision.com>
2016-03-22 14:18:18 -0400

    Add horizontal scrolling

---
+++ test/data/scroller.js
@@ -0,0 +1,20 @@
+var items = [
+  {
+    "text": "Did Pluto have rivers and lakes?<br clear='all'/><br/><br/>",
+    "fontStyle": {
+      "align": "left",
+      "backcolor": "transparent",
+      "bold": true,
+      "customSize": "",
+      "font": {
+        "family": "verdana,geneva,sans-serif",
+        "type": "standard",
+        "url": ""
+      },
+      "forecolor": "#FF0000",
+      "italic": true,
+      "size": "36px",
+      "underline": false
+    }
+  }
+];
