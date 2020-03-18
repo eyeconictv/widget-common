066b031a9ac71904309e8ba94cc6425a93d2d4e5
Stuart Lees <stuartlees@gmail.com>
2020-03-18 13:54:16 -0400

    Test coverage

---
+++ test/data/rise-google-sheet/sheet.js
@@ -0,0 +1,15 @@
+/* exported sheetData, sheetKey */
+
+var sheetData = {
+    "majorDimension": "ROWS",
+    "range": "Sheet1!A1:F999",
+    "values": [
+      [ "Name", "Type", "Total" ],
+      [ "Apple", "Fruit", "0.99" ],
+      [ "Milk", "Drink", "3.99" ],
+      [ "Crackers", "Snack", "5.99" ],
+      [ new Date( 2017, 1, 15, 0, 0, 0 ), new Date( 2017, 1, 15, 0, 0, 0 ), new Date( 2017, 1, 15, 0, 0, 0 ) ]
+    ],
+    "date": new Date( 2017, 1, 15, 0, 0, 0 )
+  },
+  sheetKey = "risesheet_abc";
