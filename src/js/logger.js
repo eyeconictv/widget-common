54f18d377a72560fd2ed848ed46958e84cf42047
Tyler Johnson <mail@tyler-johnson.ca>
2020-06-24 14:49:57 -0400

    Fix linting

--- src/js/logger.js
+++ src/js/logger.js
@@ -64,27 +64,27 @@ RiseVision.Common.LoggerUtils = (function() {
   function generateUUID() { 
     var d = new Date().getTime();// Timestamp
     var d2 = (performance && performance.now && (performance.now()*1000)) || 0; //Time in microseconds since page-load or 0 if unsupported
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
       var r = Math.random() * 16; //random number between 0 and 16
       if(d > 0){ // Use timestamp until depleted
-        r = (d + r)%16 | 0;
+        r = (d + r)%16 | 0; // jshint ignore:line
         d = Math.floor(d/16);
       } else { // Use microseconds since page-load if supported
-        r = (d2 + r)%16 | 0;
+        r = (d2 + r)%16 | 0; // jshint ignore:line
         d2 = Math.floor(d2/16);
       }
-      return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+      return (c === "x" ? r : (r & 0x3 | 0x8)).toString(16); // jshint ignore:line
     });
   }
   
   function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-      name = name.replace(/[[]]/g, '\\$&');
-      var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+    if (!url) { url = window.location.href; }
+      name = name.replace(/[[]]/g, "\\$&");
+      var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
         results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    if (!results) { return null; }
+    if (!results[2]) { return ""; }
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
   }
 
   // Get suffix for BQ table name.
